baeaf4abbc850d829747979a12c97d425e84f4ec
MenJun <1817172264@qq.com>
2020-06-13 12:56:09 +0800

    12

--- SmartRental/Controllers/HomesController.cs
+++ SmartRental/Controllers/HomesController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SmartRental.Controllers
 {
+    [EnableCors(headers:"*",methods:"*", origins:"*")]
     public class HomesController : ApiController
     {
         [HttpGet]
