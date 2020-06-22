32debc4bfede934f017f9fc6630a70dfc41ae907
2271209005@qq.com <2271209005@qq.com>
2020-06-22 17:00:28 +0800

    酒店信息添加

---
+++ SmartRental/BLL/ServiceAdmin/GHotelManageService.cs
@@ -0,0 +1,17 @@
+﻿using SmartRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartRental.BLL.ServiceAdmin
+{
+    public class GHotelManageService
+    {
+        public static bool AddHotelManager(HotelManag manag, string[] photo)
+        {
+
+            return DAL.MapperAdmin.GHotelManagerMan.AddHotelManager(manag, photo) > 0;
+        }
+    }
+}
---
+++ SmartRental/DAL/MapperAdmin/GHotelManagerMan.cs
@@ -0,0 +1,60 @@
+﻿using SmartRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartRental.DAL.MapperAdmin
+{
+    public class GHotelManagerMan
+    {
+        public static int AddHotelManager(HotelManag manager, string[] photo)
+        {
+            using (SmartRentalSystemEntities db = new SmartRentalSystemEntities())
+            {
+                HotelPhoto hotel = new HotelPhoto();
+                for (int i = 0; i < photo.Length; i++)
+                {
+
+                    if (i == 0)
+                        hotel.Hotelphoto1 = photo[i].ToString();
+                    if (i == 1)
+                        hotel.Hotelphoto2 = photo[i].ToString();
+                    if (i == 2)
+                        hotel.Hotelphoto3 = photo[i].ToString();
+                    if (i == 3)
+                        hotel.Hotelphoto4 = photo[i].ToString();
+                    if (i == 4)
+                        hotel.Hotelphoto5 = photo[i].ToString();
+                    if (i == 5)
+                        hotel.Hotelphoto6 = photo[i].ToString();
+                    if (i == 6)
+                        hotel.Hotelphoto7 = photo[i].ToString();
+
+                }
+
+                db.HotelPhoto.Add(hotel);
+
+
+
+               
+                db.SaveChanges();
+            }
+            using (SmartRentalSystemEntities db = new SmartRentalSystemEntities())
+            {
+
+             
+         
+                db.HotelManag.Add(manager);
+
+                return db.SaveChanges();
+
+
+
+            }
+
+
+
+        }
+    }
+}
