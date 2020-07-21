765db8830ca609c6a8b598fd538b15522586909a
1817172264@qq.com <1817172264@qq.com>
2020-07-21 13:21:55 +0800

    数据库修改

--- SmartRental/Models/Model1.Context.cs
+++ SmartRental/Models/Model1.Context.cs
@@ -18,7 +18,6 @@ namespace SmartRental.Models
         public SmartRentalSystemEntities()
             : base("name=SmartRentalSystemEntities")
         {
-            Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
